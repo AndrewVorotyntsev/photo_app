b20d131d3b7e795981f6ba1e0cf3460e43a78a7b
Andrew Vorotyntsev <andrey.vortyntsev@mail.ru>
2023-06-21 13:29:04 +0300

    Доменная модель для фотографии

---
+++ lib/domain/image.dart
@@ -0,0 +1,17 @@
+// Доменная модель изображения
+class Photo {
+  /// Сссылка на изображение
+  final String imageUrl;
+
+  /// Имя автора
+  final String author;
+
+  /// Количество лайков
+  final int likes;
+
+  Photo({
+    required this.imageUrl,
+    required this.author,
+    required this.likes,
+  });
+}
