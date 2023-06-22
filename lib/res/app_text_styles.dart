71c7cf74a22ce9b27ea62af6498b4c824c6b22ca
Andrew Vorotyntsev <andrey.vortyntsev@mail.ru>
2023-06-22 12:51:55 +0300

    SI-4415 feat: Вынес название шрифта в локальную константу

--- lib/res/app_text_styles.dart
+++ lib/res/app_text_styles.dart
@@ -1,29 +1,31 @@
 import 'package:flutter/material.dart';
 
 class AppTextStyles {
+  static const String manropeFontName = 'Manrope';
+
   static TextStyle expandedAppbarTextStyle = const TextStyle(
-    fontFamily: 'Manrope',
+    fontFamily: manropeFontName,
     fontWeight: FontWeight.w700,
     fontSize: 24,
     color: Colors.black,
   );
 
   static TextStyle collapsedAppbarTextStyle = const TextStyle(
-    fontFamily: 'Manrope',
+    fontFamily: manropeFontName,
     fontWeight: FontWeight.w700,
     fontSize: 20,
     color: Colors.black,
   );
 
   static TextStyle cardTitleTextStyle = const TextStyle(
-    fontFamily: 'Manrope',
+    fontFamily: manropeFontName,
     fontWeight: FontWeight.w700,
     fontSize: 12,
     color: Colors.white,
   );
 
   static TextStyle cardSubtitleTextStyle = const TextStyle(
-    fontFamily: 'Manrope',
+    fontFamily: manropeFontName,
     fontWeight: FontWeight.w400,
     fontSize: 10,
     color: Colors.white,
