3d2c7f33c93ffc093006eac30bac76aa0e0a646c
Andrew Vorotyntsev <andrey.vortyntsev@mail.ru>
2023-06-22 18:55:39 +0300

    SI-4415 feat(list): Блюр на основе хэша во время загрузки изображений

--- lib/ui/widgets/photo_card.dart
+++ lib/ui/widgets/photo_card.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blurhash/flutter_blurhash.dart';
 import 'package:photo_app/domain/photo.dart';
-import 'package:photo_app/res/app_colors.dart';
 import 'package:photo_app/res/app_strings.dart';
 import 'package:photo_app/res/app_text_styles.dart';
 
@@ -17,54 +17,87 @@ class PhotoCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.centerLeft,
+      // Для того, чтобы изображение заняло всю карточку.
+      fit: StackFit.expand,
+      children: [
+        BlurPlaceholder(blurHash: photo.blurHash),
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(
+              Radius.circular(
+                20,
+              ),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: photo.shadowColor,
+                blurRadius: 32,
+                offset: const Offset(0, 8),
+              ),
+            ],
+            image: DecorationImage(
+              image: NetworkImage(photo.imageUrl),
+              fit: BoxFit.fill,
+            ),
+          ),
+          clipBehavior: Clip.hardEdge,
+          child: Material(
+            type: MaterialType.transparency,
+            clipBehavior: Clip.hardEdge,
+            child: InkWell(
+              onTap: onCardTap,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      photo.author,
+                      style: AppTextStyles.cardTitleTextStyle,
+                    ),
+                    Text(
+                      '${photo.likes} ${AppStrings.likes}',
+                      style: AppTextStyles.cardSubtitleTextStyle,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+/// Виджет с блюр эффектом на основе кэша.
+/// Отображается до момента загрузки изображения.
+class BlurPlaceholder extends StatelessWidget {
+  final String blurHash;
+
+  const BlurPlaceholder({
+    required this.blurHash,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        borderRadius: const BorderRadius.all(
+      clipBehavior: Clip.hardEdge,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(
           Radius.circular(
             20,
           ),
         ),
-        boxShadow: [
-          BoxShadow(
-            color: photo.shadowColor,
-            blurRadius: 32,
-            offset: const Offset(0, 8),
-          ),
-        ],
-        color: AppColors.cardColor,
-        image: DecorationImage(
-          image: NetworkImage(photo.imageUrl),
-          fit: BoxFit.fill,
-        ),
       ),
-      clipBehavior: Clip.hardEdge,
-      child: Material(
-        type: MaterialType.transparency,
-        clipBehavior: Clip.hardEdge,
-        child: InkWell(
-          onTap: onCardTap,
-          child: Padding(
-            padding: const EdgeInsets.only(left: 16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  photo.author,
-                  style: AppTextStyles.cardTitleTextStyle,
-                ),
-                Text(
-                  '${photo.likes} ${AppStrings.likes}',
-                  style: AppTextStyles.cardSubtitleTextStyle,
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+      child: BlurHash(hash: blurHash),
     );
   }
 }
